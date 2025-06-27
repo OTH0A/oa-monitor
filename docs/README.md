f8e83b96ab994168e11b446ba2d5ea58e922294e
OTH0A <114746207+OTH0A@users.noreply.github.com>
2025-06-27 18:10:51 +0200

    0

---
+++ docs/README.md
@@ -0,0 +1,3 @@
+<img src="../assets/MonitorBanner.png" alt="OAlogo" style="max-height: 100px">
+
+# oa-monitor
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games'gta5'
+
+author 'John Doe <j.doe@example.com>'
+description '...'
+version '0.0.1'
+
+-- What to run
+client_scripts './client/main.lua'
+
+server_script './server/main.lua'
